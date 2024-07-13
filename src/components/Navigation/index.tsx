93d66f2ea5dfc5cc21e4e42962a644d12159acfc
JM <142030205+jamadorg@users.noreply.github.com>
2024-07-13 19:58:01 +0200

    Delete wetransfer_wheat-music_2024-07-03_1550.zip

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,39 @@
+import * as S from "./styled";
+import { useState, type FC } from "react";
+import { Hamburger } from "./components/Hamburger";
+
+export const Navigation: FC = () => {
+    // to update values for all components, keep the state here
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <S.NavigationStyled>
+            <S.NavigationListWrapper $isOpen={isOpen}>
+                <S.NavigationList>
+                    <li>
+                        <a href="/">Wheat</a>
+                    </li>
+                    <li>
+                        <a
+                            href="#"
+                        >
+                            Eventos
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/">Tickets</a>
+                    </li>
+                    <li>
+                        <a href="/contact">Contacto</a>
+                    </li>
+                </S.NavigationList>
+            </S.NavigationListWrapper>
+            <Hamburger
+                state={{
+                    open: isOpen,
+                    setOpen: setIsOpen,
+                }}
+            />
+        </S.NavigationStyled>
+    );
+};
